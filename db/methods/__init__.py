bee386364e23a49cae06eff5351d6fa4a41b7b3e
Mohammad Yasin Khosh manesh <yasin.dev81@gmail.com>
2024-11-04 15:46:52 +0330

    v0.1
    - create db

--- db/methods/__init__.py
+++ db/methods/__init__.py
@@ -1,22 +1,8 @@
 from . import (
-    config as config_db,
-    ct as ct_db,
-    discount as discount_db,
-    plan as plan_db,
-    primum_servers as primum_servers_db,
     user as user_db,
-    utils as utils_db,
-    wt as wt_db,
 )
 
 
 __all__ = (
-    "config_db",
-    "ct_db",
-    "discount_db",
-    "plan_db",
-    "primum_servers_db",
     "user_db",
-    "utils_db",
-    "wt_db",
 )
