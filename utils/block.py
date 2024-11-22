91ba41fb18e5da005a6842ba036638e32d1c9ca9
Mohammad Yasin Khosh manesh <yasin.dev81@gmail.com>
2024-11-22 22:35:56 +0330

    v1.8
    - add menu to block

--- utils/block.py
+++ utils/block.py
@@ -1,10 +1,13 @@
 import glv
+from keyboards import get_main_menu_keyboard
 
 
 async def block_user(user_id: int):
     try:
         await glv.bot.send_message(
-            user_id, "شما به علت رعایت نکردن قوانین بات از طرف مدیر گروه بلاک شدید."
+            user_id,
+            "شما به علت رعایت نکردن قوانین بات از طرف مدیر گروه بلاک شدید.",
+            reply_markup=get_main_menu_keyboard(4),
         )
         # await glv.bot.ban_chat_member(chat_id=user_id, user_id=user_id)
     except Exception:
